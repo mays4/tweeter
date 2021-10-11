3679dcc2101a3bb71067e67680acebbaf84dc066
mays4 <maysalani84@yahoo.com>
2021-10-11 12:52:10 -0400

    fixed some issues

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,20 @@
 
-//  import $ from "jquery";
-
 $(document).ready(function() {
   // --- our code goes here ---
-  let count = 0;
+ 
   $('.new-tweet textarea').on("keyup",function() {
 
     let count = $(this).val().length;
-    $('.counter').text(count);
+    
     if (count > 140) {
-      $('.counter').text(140 - count);
+      
       $('.counter').css('color','red');
     } else{
       $('.counter').css('color','black');
     }
-      
+    $('.counter').text(140 - count);
+    
   });
+  
 });
 
